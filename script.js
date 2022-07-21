29fc1cd80073742ce6c11b12c7c2053a304e463f
Niteen <niteensahni@gmai.com>
2022-07-21 12:05:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function Mark1()
+{
+   document.getElementById("Mark2").src = "images/ironman_PNG90.png";
+   
+}
+function Marc2()
+{
+   document.getElementById("Mark2").src = "images/marc85.png";
+   
+}
+function hydra()
+{
+   document.getElementById("hydra").src = "images/hydra.png";
+   
+}
+function america()
+{
+   document.getElementById("hydra").src = "images/captain_america_PNG62.png";
+   
+}
+function prof()
+{
+   document.getElementById("prof").src = "images/prof.png";
+   
+}
+function inc()
+{
+   document.getElementById("prof").src = "images/hulk_PNG116.png";
+   
+}
